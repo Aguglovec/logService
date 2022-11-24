ec874d0ba94ec4a00d7c351cae1e8b480c951dfc
yevgeny alianov <alianov@gmail.com>
2022-11-24 16:30:22 +0200

    missing scripts  - alipine.js - switch to CDN  - resolve later

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,11 @@
-import 'alpinejs'
+// import alpinejs from 'alpinejs'
+// window.alpinejs = alpinejs
+// alpinejs.start()
+
+import alpinejs from 'https://cdn.skypack.dev/alpinejs';
+// import * as comps from '/dist/demo/index.js';
+// Object.keys(comps).forEach(comp => {
+//     let data = comps[comp]();
+//     alpinejs.data(comp, () => data);
+// });
+alpinejs.start();
